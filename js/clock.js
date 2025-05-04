75afd48ac874346b5c5b9845c8b08ebccf7c1052
CHECHO2 <mijjn@naver.com>
2025-05-05 01:04:44 +0900

    ~#8 CSS

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,15 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  let hours = date.getHours();
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const ampm = hours >= 12 ? "午後" : "午前";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  clock.innerText = `${ampm} ${String(hours).padStart(2, "0")}:${minutes}`;
 }
 
 getClock();
